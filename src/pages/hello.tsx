21a89bf5a4dd7d323a74a8378a0c568b2956b5ce
Mic Ruopp <micruopp@gmail.com>
2025-12-06 15:02:51 -0600

    feat: add HelloPage component with layout

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,2 +1,13 @@
 // hello.tsx
 // A test page.
+
+import Layout from '../components/layout';
+
+export default function HelloPage() {
+  let pageName = "hello";
+  return (
+    <Layout pageName={pageName}>
+      <p>hello, world!</p>
+    </Layout>
+  );
+}
